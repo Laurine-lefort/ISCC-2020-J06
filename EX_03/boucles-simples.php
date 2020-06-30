c55169a3ad10cf4f653e3ad50f2757278e4a190c
Laurine-lefort <laurine.le-fort@iseg.fr>
2020-06-30 17:05:50 +0200

    jour 6

---
+++ EX_03/boucles-simples.php
@@ -0,0 +1,19 @@
+<meta charset="utf-8">
+
+<?php
+
+function compter($maximum)
+{
+    while($maximum) {
+        $maximum=$maximum + 1;
+    }
+    echo"$maximum";
+    return;
+}
+compter(10);
+function compter_for()
+{
+    echo"";
+    return;
+}
+?>

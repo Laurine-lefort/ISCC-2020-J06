bf09c17b81c0b5adf3d5629814a15f50955ca12a
Laurine-lefort <laurine.le-fort@iseg.fr>
2020-06-30 10:11:38 +0200

    jour 6

---
+++ EX_01/test-fonctions.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+<?php
+  $nom_produit='T-shirt femme';
+  $couleur='Rouge';
+  $prix=15.50;
+  $disponible=true;
+  $quantité=10;
